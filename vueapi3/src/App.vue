85ef65e3c05817cccc6e354e60b227207d7bc9e5
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-04-04 13:58:26 -0400

    1

--- vueapi3/src/App.vue
+++ vueapi3/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import testpage from './components/testapi.vue'
 </script>
 
 <template>
---
+++ vueapi3/src/components/testapi.vue
@@ -0,0 +1,8 @@
+<template>
+
+</template>
+<script setup>
+async function api() {
+    let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+}
+</script>
