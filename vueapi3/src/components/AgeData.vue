7e6696fa30bf417999aba0c4d9099ec76bd2af23
AndrewL777 <7landrew7@gmail.com>
2023-04-19 14:01:12 -0400

    Create AgeData.vue

---
+++ vueapi3/src/components/AgeData.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>hello</h1>
+</template>
+<script setup>
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+
+// let allrace = []
+
+async function getapi() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
+  let data = await res.json()
+  console.log(data.perp_race)
+}
+
+getapi()
+
+// let i = 1
+// if(data[i].perp_race != data[i-0]){
+//     allrace.push(data)
+// }
+// }
+// onMounted(() =>{
+//     getapi()
+//     console.log(allrace)
+//     console.log("hfjdcndncdc")
+// })
+</script>
