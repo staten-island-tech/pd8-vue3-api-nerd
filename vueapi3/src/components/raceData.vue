68ae43f22497b07a48dddf385a5ec6ebcb8e3345
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-04-20 13:56:27 -0400

    1

--- vueapi3/src/components/raceData.vue
+++ vueapi3/src/components/raceData.vue
@@ -13,12 +13,12 @@ let whitehispanic = 0
 let americanindianAlaskanNative = 0
 let unknown = 0
 
-let info = []
+
 
 async function getapi(){
     let res = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
     let data = await res.json()
-    info.push(data)
+
     allrace.push(data[0].perp_race)
     let i = 1
     while(i <= 1000){
@@ -31,14 +31,8 @@ async function getapi(){
     }
 }
 
-function test(){
-    
-}
-
 onMounted(() =>{    
     getapi()
     console.log(allrace)
-    console.log(info)
-    test()
 })
 </script>
