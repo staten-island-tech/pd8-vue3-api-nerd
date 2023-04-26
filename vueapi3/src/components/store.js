5c9fcab6ed42017b68eb614eecd04854ead22f19
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-04-26 13:51:39 -0400

    1

--- vueapi3/src/components/store.js
+++
@@ -1,7 +0,0 @@
-import { reactive } from 'vue'
-
-export const api = 
-    async function(){
-    let res = await fetch("https://data.cityofnewyork.us/resource/uip8-fykc.json")
-    data = await res.json()
-}
