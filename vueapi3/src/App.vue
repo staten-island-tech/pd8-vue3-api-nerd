4207a00e8e47d019ccd750b5fcde35efee0d6c24
AndrewL777 <7landrew7@gmail.com>
2023-04-26 13:38:26 -0400

    Revert "1"
    This reverts commit e50520ca56a1783f688fd591d05bd291ec4eed4e.

--- vueapi3/src/App.vue
+++ vueapi3/src/App.vue
@@ -3,7 +3,11 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header></header>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div></div>
+  </header>
 
   <RouterView />
 </template>
