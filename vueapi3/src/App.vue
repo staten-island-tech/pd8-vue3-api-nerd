e50520ca56a1783f688fd591d05bd291ec4eed4e
AndrewL777 <7landrew7@gmail.com>
2023-04-26 13:36:08 -0400

    1

--- vueapi3/src/App.vue
+++ vueapi3/src/App.vue
@@ -3,11 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div></div>
-  </header>
+  <header></header>
 
   <RouterView />
 </template>
