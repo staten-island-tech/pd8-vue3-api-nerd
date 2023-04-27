b7fe5c4eedb4b380a1e412646d7dcb2c44fbef0d
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-04-27 13:20:48 -0400

    1

--- api/src/App.vue
+++ api/src/App.vue
@@ -2,77 +2,23 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HomePage from './views/HomePage.vue'
 import RacePage from './views/RacePage.vue'
+
+let currentpage = "home"
 </script>
 
 <template>
   <header>
-    <HomePage/>
-    <RacePage/>
+    <div v-if="currentpage === 'home'">
+      <HomePage/>
+    </div>
+    <div v-if="currentpage === 'race'">
+      <RacePage/>
+    </div>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- api/src/views/HomePage.vue
+++ api/src/views/HomePage.vue
@@ -1,7 +1,11 @@
 <script setup>
+const page = "hme"
 </script>
 <template>
-
+<div v-if="page === 'home'">
+    <h1>hellojdjd</h1>
+</div>
 </template>
 <style scoped>
+
 </style>
