131d76e2691cbfee9fed9512e6802c09476d2cfd
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-04-28 16:49:55 -0400

    q

--- api/src/views/RacePage.vue
+++ api/src/views/RacePage.vue
@@ -1,7 +1,51 @@
-<script setup></script>
+<script>
+import { ref, onMounted } from 'vue'
+
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: ['Brooklyn', 'Staten Island', 'Manhattan', 'Queen', 'Bronx'],
+        datasets: [{ data: [40, 20, 12] }]
+      },
+      chartOptions: {
+        responsive: true
+      },
+      BK: 0,
+      SI: 0,
+      MH: 0,
+      QN: 0,
+      BX: 0
+    }
+  },
+  methods: {
+    getapi: async function () {
+      let res = import('https://data.cityofnewyork.us/resource/kwvk-z7i9.json')
+      let data = await res.json()
+      console.log(data)
+    }
+  },
+  mounted: {}
+}
+</script>
 
 <template>
-  <h1>hello this is racepage</h1>
+  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
 
 <style scoped></style>
