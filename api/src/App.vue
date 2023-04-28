b158e31d2001f46a7868f6202603b25cb8977080
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-04-28 13:53:11 -0400

    1

---
+++ api/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink class="btn" to="/home">Home</RouterLink>
+        <RouterLink class="btn" to="/race">About</RouterLink>
+      </nav>
+  
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.btn{
+  text-align: center;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+</style>
---
+++ api/src/views/HomePage.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+
+<template>
+<h1>hello this is homepage</h1>
+</template>
+
+<style scoped></style>
