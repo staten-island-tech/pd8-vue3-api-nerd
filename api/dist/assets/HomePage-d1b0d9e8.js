87888d5a20a7b3f6576d14ef20da34b8a8d3d877
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-05-01 16:07:26 -0400

    1

---
+++ api/dist/assets/HomePage-d1b0d9e8.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-4a441ee9.js";const t={};function n(r,s){return o(),c("h1",null,"hello this is homepage")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ api/dist/assets/RacePage-4f9f8e5e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as a}from"./index-4a441ee9.js";const o={};function r(t,n){return c(),a("h1",null,"hello this is racepage")}const _=e(o,[["render",r]]);export{_ as default};
