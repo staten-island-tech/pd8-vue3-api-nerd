4193aedddb984a09866d2cfb4e115426f9d0875d
AndrewL777 <7landrew7@gmail.com>
2023-05-01 13:38:41 -0400

    11

---
+++ charts/src/components/CHARTRACESBACKUP.vue
@@ -0,0 +1,85 @@
+<!-- <template>
+  <canvas id="myChart" width="500" height="500"></canvas>
+
+</template>
+
+<script>
+
+const APIURL = "https://data.cityofnewyork.us/resource/uip8-fykc.json";
+
+async function CheckF() {
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  let females = 0;
+  Info.forEach((person) => {
+    if (person.perp_sex.includes("F")) {
+      females++;
+    }
+  });
+  console.log(females)
+  return females;
+}
+
+async function CheckM() {
+  const response = await fetch(APIURL);
+  const Info = await response.json();
+  let males = 0;
+  Info.forEach((person) => {
+    if (person.perp_sex.includes("M")) {
+      males++;
+    }});
+    console.log(males)
+    return males;
+}
+
+
+
+CheckF()
+
+CheckM()
+
+async function getData() {
+  const males = await CheckM();
+  const females = await CheckF();
+  return [males, females];
+}
+
+
+
+import Chart from 'chart.js/auto';
+
+
+async function createChart() {
+  const [males, females] = await getData();
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Males', 'Females'],
+      datasets: [{
+        label: 'Arrest Datas for Genders',
+        data: [males, females],
+        backgroundColor: ['blue',]
+      }]
+    },
+    options: {
+    maintainAspectRatio: true,
+    aspectRatio: 1,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+
+          }
+        }]
+      }
+    }
+  });
+
+}
+
+createChart()
+
+
+</script> -->
+<script></script>
